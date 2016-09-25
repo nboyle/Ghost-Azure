d08254106bc3c3ef7c682009bd1c6993eb1f19c0
Nate Boyle <nboyle@ikkostraining.com>
2016-09-25 14:43:45 -0400

    Add files via upload

--- content/themes/ghost/assets/js/mulada.js
+++ content/themes/ghost/assets/js/mulada.js
@@ -20,13 +20,14 @@ $(function(){
 		if(fbroot == "") {
 		
 		} else {
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.7&appId=809088432478669";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+			(function(d, s, id) {
+				var js, fjs = d.getElementsByTagName(s)[0];
+				if (d.getElementById(id)) return;
+				js = d.createElement(s); js.id = id;
+				js.src = "//connect.facebook.net/tr_TR/sdk.js#xfbml=1&version=v2.3&appId=867298396696336";
+				fjs.parentNode.insertBefore(js, fjs);
+			}
+			(document, 'script', 'facebook-jssdk'));
 		}
 
 		/*---------- OWL CAROUSEL ----------*/
